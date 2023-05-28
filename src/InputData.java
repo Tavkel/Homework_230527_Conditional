4a97f59e969250230f837b6ab306d94a2d2d3cfa
Tavkel <gmininb@gmail.com>
2023-05-28 17:59:02 +0300

    Tasks 1-3 complete

---
+++ src/InputData.java
@@ -0,0 +1,19 @@
+public class InputData
+{
+	private int _age = 65;
+	private int _temperature = 12;
+	private int _speed = 56;
+
+	public int GetAge()
+	{
+		return this._age;
+	}
+	public int GetTemperature()
+	{
+		return this._temperature;
+	}
+	public int GetSpeed()
+	{
+		return this._speed;
+	}
+}
